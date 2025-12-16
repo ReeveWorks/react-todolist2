173d34169e9b150d5a8a61522d384ac234fc9e28
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2025-12-16 08:38:09 +0800

    v0.2 Calling the Components
    In App.jsx, referenced/called the component header, todolist, and footer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import { useState } from 'react'
+import Header from './components/header.jsx'
+import TodoList from './components/todolist.jsx'
+import Footer from './components/footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <Header/>
+      <TodoList/>
+      <Footer/>
     </>
   )
 }
