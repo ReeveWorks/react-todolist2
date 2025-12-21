07e71eb279dfe1790d5161b418e651a9e05a55e8
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2025-12-21 10:42:42 +0800

    v0.7 Experimenting with useState
    Trying to understand useState Hook

--- src/components/todolist.jsx
+++ src/components/todolist.jsx
@@ -1,10 +1,34 @@
 import '../styles/todolist.css'
+import { useState } from 'react';
 
 function todolist() {
+
+  const [lists, setList] = useState("Item 0");
+  const [item, setItem] = useState("");
+
+  function InputChange(event) {
+    setItem(event.target.value);
+  }
+
+  function addItem() {
+    if(item.trim() === "") return;
+
+    setList(item);
+    setItem("");
+  }
 
   return (
     <>
       <div className="todolist">
+        <input 
+          type="text" 
+          value={item} 
+          onChange={InputChange}
+          placeholder="Add new task..." />
+        <button onClick={() => addItem()}>
+          Add
+        </button>
+        <p>{lists}</p>
         <p>Item 1</p>
         <p>Item 2</p>
         <p>Item 3</p>
