eb18db1ac384eb46d249c65d0addbdc0d742a6fd
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-01-04 15:39:01 +0800

    v1.8 Object Update

--- src/store/todoItems.jsx
+++ src/store/todoItems.jsx
@@ -1,7 +1,16 @@
 import { create } from "zustand";
 
+// type TodoStore = {
+//   tasks: string,
+//   weight: number,
+// };
+
 const useTodosStore = create((set) => ({
-  tasks: ["Sample Task 1", "Sample Task 2"],
+  tasks: [
+    {tasks: "Learn Zustand", weight: 10},
+    {tasks: "Build a Todo App", weight: 38},
+    {tasks: "Profit", weight: 15},
+  ],
   addTodo: (newTask) => set((state) => ({ tasks: [...state.tasks, newTask] })),
   setTasks: (newTasks) => set({ tasks: newTasks}),
 }));
