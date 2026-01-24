fe6a1e2ee6d25f2bbfabf48c7e1e5ab1b4f01863
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-01-24 11:17:32 +0800

    v3.8 Redux Update

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import TodoList from './components/todolist.jsx'
 import Footer from './components/footer.jsx'
 import { TasksProvider } from './contexts/tasksContext.jsx'
 import { useState } from 'react'
+import {Provider} from 'react-redux'
+import {store} from './state/store.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="ctr-app">
-      <TasksProvider>
+      <Provider store={store}>
         <div className='ctr-header'><Header/></div>
         <div className='ctr-todolist'><TodoList/></div>
         <div className='ctr-footer'><Footer/></div>
-      </TasksProvider>
+      </Provider>
     </div>
   )
 }
--- src/state/todolist/todoSlice.jsx
+++ src/state/todolist/todoSlice.jsx
@@ -2,13 +2,26 @@ import { createSlice } from '@reduxjs/toolkit';
 
 
 const initialState = {
-  value: 0,
+  todos: [
+    {id: 1, task: "Learn Redux", weight: 55, status: false},
+    {id: 2, task: "Master JavaScript", weight: 7, status: false},
+    {id: 3, task: "Build a App", weight: 6, status: false},
+    {id: 4, task: "Procrastinating 2!", weight: 1, status: true},
+  ],
 };
 
 const todoSlice = createSlice({
-  name: 'todo',
+  name: 'todos',
   initialState,
-  reducers: {}
+  reducers: {
+    addtask : (state, action) => {
+      state.todos.push(action.payload);
+    },
+    deletetask : (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    }
+  }
 });
 
+export const { addtask, deletetask } = todoSlice.actions;
 export default todoSlice.reducer;
