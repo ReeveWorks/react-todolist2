20d4e0010c1bbc75b61ce6e92bb161d52c29a041
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-01-25 13:03:59 +0800

    v3.9 Setting up management pages
    Creating different file for different data management

---
+++ src/components/todo-context.jsx
@@ -0,0 +1,4 @@
+import '../styles/global.css'
+import '../styles/todolist.css'
+import { useState, useContext } from 'react';
+import { TasksContext } from '../contexts/tasksContext.jsx';
---
+++ src/components/todo-zustand.jsx
@@ -0,0 +1,4 @@
+import '../styles/global.css'
+import '../styles/todolist.css'
+import { useState } from 'react';
+import { useTodosStore } from '../store/todoItems.jsx';
