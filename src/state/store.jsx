7519c0d7fb02b9bbd5ccea2c28b09b664490c5a9
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-01-30 12:03:51 +0800

    v4.4 Update

--- src/state/store.jsx
+++ src/state/store.jsx
@@ -3,9 +3,13 @@ import todoReducer from "./todolist/todoSlice";
 
 export const store = configureStore({
   reducer: {
-    // Add your reducers here
+    todos: todoReducer,
   },
 });
 
-export const RootState = ReturnType(store.getState);
+/**
+ * Returns the current Redux state at runtime.
+ * Use `getRootState()` to access the latest state from the store.
+ */
+export const getRootState = () => store.getState();
 export const AppDispatch = store.dispatch;
