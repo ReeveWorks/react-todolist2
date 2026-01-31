4ab9cb1f4c76ee485b85c06fc26e2e916529b9cd
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-01-31 12:20:24 +0800

    v4.5 Redux - todolist added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './styles/global.css'
 import './styles/app.css'
 import Header from './components/header.jsx'
-import TodoList from './components/todolist.jsx'
 import Footer from './components/footer.jsx'
-import { TasksProvider } from './contexts/tasksContext.jsx'
 import { useState } from 'react'
 import {Provider} from 'react-redux'
 import {store} from './state/store.jsx'
+import { TasksProvider } from './contexts/tasksContext.jsx'
+
+import TodoList from './components/todolist.jsx'
 import todolistZustand from './components/todo-zustand.jsx'
+import TodoListRedux from './components/todo-redux.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -24,7 +26,7 @@ function App() {
 
       {/* redux management system */}
       <Provider store={store}>
-        <div className='ctr-todolist'><TodoList/></div>
+        <div className='ctr-todolist'><TodoListRedux/></div>
       </Provider>
       
       <div className='ctr-footer'><Footer/></div>
--- src/components/todo-redux.jsx
+++ src/components/todo-redux.jsx
@@ -1,3 +1,50 @@
 import '../styles/global.css'
 import '../styles/todolist.css'
 import { useState } from 'react';
+import {useSelector}  from 'react-redux';
+
+function todolist() {
+    const tasks = useSelector((state) => state.todos.todos);
+  
+
+  return (
+    <>
+      <div className="todolist">
+        <ol className='lst-task'>
+          {
+            tasks
+              .filter(i => !i.status)
+              .sort((a, b) => b.weight - a.weight)
+              .map((task) => (
+                <li key={task.id}>
+                  <b>{task.weight}&nbsp;</b>
+                  <span>{task.task}</span>
+
+                  <button>✓</button>
+                  <button>✕</button>
+                </li>
+              ))
+          }
+        </ol>
+        <ol className='lst-completed'>
+          {
+            tasks
+              .filter(i => i.status)
+              .sort((a, b) => b.weight - a.weight)
+              .map((task) => (
+                <li key={task.id}>
+                  <b>{task.weight}&nbsp;</b>
+                  <span>{task.task}</span>
+
+                  <button>✓</button>
+                  <button>✕</button>
+                </li>
+              ))
+          }
+        </ol>
+      </div>
+    </>
+  )
+}
+
+export default todolist
