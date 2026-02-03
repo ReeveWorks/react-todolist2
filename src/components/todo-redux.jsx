817524d968ff3ffc1c7065b5d656d2a8524ee8b0
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-02-03 11:55:30 +0800

    v4.7 Redux - todolist add task

--- src/components/todo-redux.jsx
+++ src/components/todo-redux.jsx
@@ -1,24 +1,67 @@
-import { toggletask, deletetask } from '../state/todolist/todoSlice';
+import { toggletask, deletetask, addtask } from '../state/todolist/todoSlice';
 import '../styles/global.css'
 import '../styles/todolist.css'
 import { useState } from 'react';
-import {useSelector, useDispatch}  from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function todolist() {
+  const [item, setItem] = useState("");
+  const [weight, setWeight] = useState(0);
+
+  function InputChange(event) {
+    setItem(event.target.value);
+  }
+  function WeightChange(event) {
+    if (event.target.value < 0) return;
+    if (event.target.value > 10) return;
+    if (event.target.value.includes('.')) return;
+
+    setWeight(event.target.value);
+  }
+
   const tasks = useSelector((state) => state.todos.todos);
   const dispatch = useDispatch();
 
   function toggleStatus(index) {
     dispatch(toggletask(index));
   }
-  
+
   function deleteTask(index) {
     dispatch(deletetask(index));
+  }
+
+  function addTask() {
+    dispatch(addtask(item));
+
+    setItem("");
+    setWeight(0);
   }
 
   return (
     <>
       <div className="todolist">
+        <>
+          <input
+            type="text"
+            value={item}
+            onChange={InputChange}
+            placeholder="Add new task..." />
+          <input
+            type="number"
+            value={weight}
+            onChange={WeightChange}
+            placeholder="set weight"
+            step='1'
+            min='0'
+            max='100' />
+
+          <button 
+            className='btn-Add' 
+            onClick={() => addTask()}
+            >Add</button>
+        </>
+
+
         <ol className='lst-task'>
           {
             tasks
