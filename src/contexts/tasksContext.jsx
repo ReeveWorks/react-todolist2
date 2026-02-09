2b1a2c69a9bd5b796a425ad79653d87a203ea4bb
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-02-09 15:55:26 +0800

    v5.4 UseContext Move Functions

--- src/contexts/tasksContext.jsx
+++ src/contexts/tasksContext.jsx
@@ -13,7 +13,7 @@ export const TasksProvider = ({ children }) => {
   ]);
 
   function addTodo(task) {
-    setTodo([...todo, { id: todo[todo.length - 1].id + 1, task, status: false }]);
+    todo.push({ id: todo[todo.length - 1].id + 1, task, status: false });
   }
   function toggleTodo(task) {
     setTodo(todo.map((todo) => 
@@ -21,8 +21,24 @@ export const TasksProvider = ({ children }) => {
     ));
   }
 
+  function moveUp(task) {
+    if (task === 1) return; // Can't move up the first item
+
+    setTodo(todo.map((todo) => 
+      todo.id === task ? { ...todo, id: task-1 } : todo.id === task-1 ? { ...todo, id: task } : todo
+    ));
+  }
+
+  function moveDown(task) {
+    if (task === todo.length) return; // Can't move down the last item
+
+    setTodo(todo.map((todo) => 
+      todo.id === task ? { ...todo, id: task+1 } : todo.id === task+1 ? { ...todo, id: task } : todo
+    ));
+  }
+
   return (
-    <TasksContext.Provider value={{ todo, addTodo, setTodo, toggleTodo }}>
+    <TasksContext.Provider value={{ todo, addTodo, setTodo, toggleTodo, moveUp, moveDown }}>
       {children}
     </TasksContext.Provider>
   );
