24ee73ef45ec3bd74b37b79d235f76bab8f8c3f4
EndPlay <128982259+ReeveWorks@users.noreply.github.com>
2026-02-16 15:16:07 +0800

    v6.1 Style Update

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,15 @@
 import '../styles/global.css'
+import gitIcon from '../assets/github.svg';
+import '../styles/footer.css'
+
 
 function footer() {
   return (
     <>
       <footer className="footer">
-        <p>Hi this is a footer...</p>
+        <li>
+          <a href='https://github.com/ReeveWorks'><img src={gitIcon} alt="github" className="github"/>Hi! Click to check my GitHub Account!</a>
+        </li>
       </footer>
     </>
   )
